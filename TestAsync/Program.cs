1220f5e2560a0117e443d079c625b9ecf0326de8
Michael Nguyen <mn@3dsafety.com.au>
2019-07-11 16:19:14 +1000

    initial commit

---
+++ TestAsync/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace TestAsync
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var sw = Stopwatch.StartNew();
+                for (var i = 0; i < 10; i++)
+                {
+                    await TestAsyncParallel();
+                    sw.Stop();
+                    Console.WriteLine($"Took {sw.ElapsedMilliseconds}ms");
+                    sw.Restart();
+                }
+                Console.ReadKey();
+            }).GetAwaiter().GetResult();
+        }
+
+        private static async Task TestAsyncSequential()
+        {
+            await EmptyTask();
+            await EmptyTask();
+            await EmptyTask();
+        }
+
+        private static async Task TestAsyncParallel()
+        {
+            await Task.WhenAll(EmptyTask(), EmptyTask(), EmptyTask());
+        }
+
+        private static async Task EmptyTask()
+        {
+            await Task.Delay(1000);
+        }
+    }
+}
